9821761a876e47337b2295241a264b9ca593308a
Ali A.karim Ali <ali-karim99@hotmail.com>
2024-09-29 12:39:23 +0300

    update sarver.js

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 // Load Dependencies
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose  =  require('mongoose');
+const  expressLayouts = require('express-ejs-layouts');
 
-require('dotenv').config()
+const session = require('express-session');
+const passport = require('passport');
 
-const PORT = process.env.PORT
+// Require and Initialize dotenv
+require('dotenv').config();
 
-const app = express()
+// PORT Configuration
+const PORT = process.env.PORT;
 
-const db = require('./config/db')
+// Initialize Express
+const app = express();
+
+require('./config/passport');
+
+// Look for static file in the public folder.
+// (CSS, JS,  Image, Video, Audio)
+app.use(express.static("public"));
+
+// Database Configuration
+const db = require('./config/db');
+
+// Nodejs to look in a folder called views for all the ejs files
+app.set("view engine", "ejs");
+
+// Look in views folder for a file named layout.ejs
+app.use(expressLayouts);
+
+// Passport and Session configurations
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Share the information with other pages
+app.use(function (req, res, next){
+  res.locals.user = req.user;
+  next();
+});
 
 // Listen for all HTTP Requests on PORT 4000
 app.listen(PORT, () => {
-  console.log(`Recipe App is running on PORT ${PORT}`)
+  console.log(`Blog App is running on PORT ${PORT}`);
 })
