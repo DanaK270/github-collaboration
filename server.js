63f957a470827a81e016a368f157c4f77c619b34
mohamed_7 <moodo1999712@gmail.com>
2024-09-30 09:24:32 +0300

    just save

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.use('/', authRouter)
 
 // Listen for all HTTP Requests on PORT 4000
 app.listen(PORT, () => {
-  console.log(`Recipe App is running on PORT ${PORT}`)
+  console.log(`Book Shop App is running on PORT ${PORT}`)
 })
