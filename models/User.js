3e240a4dee54295dc39c54b702e344388ffd7d2f
mohamed_7 <moodo1999712@gmail.com>
2024-10-02 15:26:30 +0300

    corect the profile edit

--- models/User.js
+++ models/User.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose')
-const Schema =  mongoose.Schema
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-
-  name: String,
-  googleId: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    name: String,
+    googleId: {
+      type: String,
+      required: true
+    },
+    email: String,
+    avatar: String,
+    number: String,
+    image: {
+      type: String,
+      default: null
+    }
   },
-  email: String,
-  avatar: String,
-}, 
-{ timestamps: true });
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('User', userSchema)
